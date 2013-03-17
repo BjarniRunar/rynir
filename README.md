d6fd24a3d54c294a978960c467e3ba24fe93bf5d
Bjarni R. Einarsson <bre@klaki.net>
2013-03-17 13:14:13 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# Alþingisrýnirinn #
+
+Þetta er kóði sem kemur vonandi til með að auðvelda fólki að fylgjast
+með því hvernig kjörnir fulltrúar þeirra haga atkvæðum sínum á Alþingi.
+
+
+
---
+++ rynir/althingi/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Flokkur(models.Model):
+  nafn = models.CharField(max_length=100)
+  stafur = models.CharField(max_length=1)
+  lysing = models.CharField(max_length=10000)
+  url_vefs = models.CharField(max_length=200)
+  url_mynd = models.CharField(max_length=200)
+
+class Thingmadur(models.Model):
+  nafn = models.CharField(max_length=100)
+  stafir = models.CharField(max_length=5)
+  url_vefs = models.CharField(max_length=200)
+  url_mynd = models.CharField(max_length=200)
+
+class Flokksseta(models.Model):
+  flokkur = models.ForeignKey(Flokkur)
+  thingmadur = models.ForeignKey(Thingmadur)
+  upphaf = models.DateTimeField('Gekk i flokkinn')
+  endir = models.DateTimeField('Gekk ur flokknum')
+
+class Fundur(models.Model):
+  dags = models.DateTimeField('Dagsetning thingfundar')
+
+class Umraeda(models.Model):
+  titill = models.CharField(max_length=200)
+  timi = models.DateTimeField('Upphaf umraedu')
+  efni = models.CharField(max_length=10000)
+  url_ferill = models.CharField(max_length=200)
+
+class Kosning(models.Model):
+  umraeda = models.ForeignKey(Umraeda)
+  titill = models.CharField(max_length=200)
+  timi = models.DateTimeField('Upphaf umraedu')
+  url_skjal = models.CharField(max_length=200)
+
+class Atkvaedi(models.Model):
+  kosning = models.ForeignKey(Kosning)
+  thingmadur = models.ForeignKey(Thingmadur)
+  atkvaedi = models.CharField(max_length=5)
+
