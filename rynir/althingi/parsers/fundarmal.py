671b39562275c96a1c61452de1d87471d788d3fd
Bjarni R. Einarsson <bre@klaki.net>
2013-03-23 21:02:46 +0000

    Progress on scraping individual adgenda items

--- rynir/althingi/parsers/fundarmal.py
+++ rynir/althingi/parsers/fundarmal.py
@@ -1,5 +1,11 @@
+import re
 from htmlparser import ScraperParserHTML
 from metaparser import RegisterScraperParser
+
+
+SATUHJA_RE = re.compile('\(([^\)]+)\) greiddu ekki')
+FJARSTADDIR_RE = re.compile('\(([^\)]+)\) fjarstaddir')
+
 
 class ScraperParserFundarmal(ScraperParserHTML):
   MATCH_URLS = ('http://www.althingi.is/altext/\d+/\d+/l\d+\.sgml$', )
@@ -11,11 +17,42 @@ class ScraperParserFundarmal(ScraperParserHTML):
     efni = soup.h2
 
     for para in soup.fetch('p'):
-      mal = para.a
-      for vote in para.fetch('dl'):
-        svar = vote.dt
-        menn = vote.dd
-        print '%s said %s about %s' % (menn, svar, mal)
+      brtt = para.a
+      vote = para.dl
+      if brtt is None and vote:
+        elem = vote.next
+        svar = None
+        ja, nei, fj, sh = [], [], [], []
+        while elem:
+          name = getattr(elem, 'name', None)
+
+          if name == 'dt':
+            svar = elem.b.string.replace('&nbsp;', '').strip()[:-1]
+
+          elif name == 'dd':
+            folk = elem.string.replace('.', '').strip().split(', ')
+            if svar.startswith('j'):
+              ja = folk
+            elif svar.startswith('n'):
+              nei = folk
+
+          elif name == 'p':
+            satuhja = SATUHJA_RE.search(elem.string or '')
+            fjarstaddir = FJARSTADDIR_RE.search(elem.string or '')
+            if satuhja:
+              sh = satuhja.group(1).replace('.', '').strip().split(', ')
+            elif fjarstaddir:
+              fj = fjarstaddir.group(1).replace('.', '').strip().split(', ')
+
+            if not (elem.string and elem.string.strip()): break
+
+          elem = elem.next
+
+        # FIXME: Create Umraeda/Kosning/Atkvaedi objects in DB
+        print 'J: %s' % ja
+        print 'N: %s' % nei
+        print 'F: %s' % fj
+        print 'S: %s' % sh
 
     return True
 
--- rynir/urls.py
+++ rynir/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # (r'^rynir/', include('rynir.foo.urls')),
     (r'^/?$',                                     'althingi.views.index'),
     (r'^fundur/(?P<fundur_id>\d+)/$',             'althingi.views.fundur'),
-    (r'^scrape/(?P<proto>https?)/(?P<domain>[^/]+)/(?P<path>.*)$',
+    (r'^scrape/(?P<proto>https?)(?::/)?/(?P<domain>[^/]+)/(?P<path>.*)$',
                                                   'althingi.scraper.scrape'),
     (r'^scrape/bootstrap$',                       'althingi.scraper.bootstrap'),
 
