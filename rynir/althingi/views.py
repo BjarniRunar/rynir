3453394431cab482f03d51f200bb0608744623f2
Bjarni R. Einarsson <bre@klaki.net>
2013-03-27 14:19:38 +0000

    Initial template work

--- rynir/althingi/views.py
+++ rynir/althingi/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, Http404
+from django.template import Context, loader
 
 import settings
 from althingi.models import *
@@ -11,7 +12,11 @@ def AccessDenied(Exception):
 ## Views ##
 
 def index(request):
-  return HttpResponse('Welcomes to the Althings!')
+  t = loader.get_template('althingi/index.html')
+  c = Context({
+    'base': settings.TEMPLATE_BASE
+  })
+  return HttpResponse(t.render(c))
 
 def fundur(request, fundur_id=None):
   return HttpResponse('Fundur: %s' % fundur_id)
--- rynir/settings.py
+++ rynir/settings.py
@@ -82,10 +82,13 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'rynir.urls'
 
+TEMPLATE_BASE = 'includes/base.html'
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    ROOT_DIR + '/rynir',
+    ROOT_DIR + '/rynir/includes'
 )
 
 INSTALLED_APPS = (
