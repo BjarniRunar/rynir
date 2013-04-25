6370fe090e10e0a338c51ae7971e21e4c2ddac48
Bjarni R. Einarsson <bre@klaki.net>
2013-04-25 10:11:46 +0000

    Less downtime

--- rynir/run-fcgi.sh
+++ rynir/run-fcgi.sh
@@ -4,7 +4,7 @@ BASEDIR=$(cd ..; pwd)
 # Cleanup and bookkeeping...
 kill $(cat $BASEDIR/data/rynir.pid)
 rm -f $BASEDIR/data/rynir.{pid,sock}
-(sleep 10; chmod go+w ../data/rynir.sock) &
+(sleep 3; chmod go+w ../data/rynir.sock) &
 
 # Start the server
 exec ./rynir-www.py runfcgi \
